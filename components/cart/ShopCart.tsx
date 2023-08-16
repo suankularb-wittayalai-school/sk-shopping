8d6b6e6f6d6ea20fbebdb82b6c290119df3c940e
Siravit Phokeed <siravitsasa@gmail.com>
2023-08-17 01:56:26 +0700

    fix: Shop Cart animation stretching artefact

--- components/cart/ShopCart.tsx
+++ components/cart/ShopCart.tsx
@@ -13,6 +13,8 @@ import {
   CardHeader,
   List,
   MaterialIcon,
+  transition,
+  useAnimationConfig,
 } from "@suankularb-components/react";
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -24,35 +26,52 @@ const ShopCart: StylableFC<{ cart: Cart }> = ({ cart, style, className }) => {
   const getLocaleString = useGetLocaleString();
 
   const { fromUUID } = shortUUID();
+  const { duration, easing } = useAnimationConfig();
+  const layoutTransition = transition(duration.medium2, easing.standard);
 
   return (
-    <motion.li layout style={{ borderRadius: 28 }} className="overflow-hidden">
+    <motion.li
+      layout
+      transition={layoutTransition}
+      style={{ borderRadius: 28 }}
+      className="overflow-hidden"
+    >
       <Card
         appearance="filled"
         style={style}
         className={cn(`divide-y-1 divide-outline !rounded-none`, className)}
       >
-        <CardHeader
-          avatar={<ShopLogo shop={shop} showBackground />}
-          title={getLocaleString(shop.name)}
-        />
-        <List>
-          {items.map((item) => (
-            <CartListingOption key={item.item.id} {...item} shopID={shop.id} />
-          ))}
-        </List>
-        <CardContent>
-          <Actions className="!mt-0">
-            <Button
-              appearance="filled"
-              icon={<MaterialIcon icon="shopping_cart_checkout" />}
-              href={`/cart/checkout/${fromUUID(shop.id)}`}
-              element={Link}
-            >
-              สั่งซื้อ
-            </Button>
-          </Actions>
-        </CardContent>
+        <motion.div layout="position" transition={layoutTransition}>
+          <CardHeader
+            avatar={<ShopLogo shop={shop} showBackground />}
+            title={getLocaleString(shop.name)}
+          />
+        </motion.div>
+        <motion.div layout="position" transition={layoutTransition}>
+          <List>
+            {items.map((item) => (
+              <CartListingOption
+                key={item.item.id}
+                {...item}
+                shopID={shop.id}
+              />
+            ))}
+          </List>
+        </motion.div>
+        <motion.div layout="position" transition={layoutTransition}>
+          <CardContent>
+            <Actions className="!mt-0">
+              <Button
+                appearance="filled"
+                icon={<MaterialIcon icon="shopping_cart_checkout" />}
+                href={`/cart/checkout/${fromUUID(shop.id)}`}
+                element={Link}
+              >
+                สั่งซื้อ
+              </Button>
+            </Actions>
+          </CardContent>
+        </motion.div>
       </Card>
     </motion.li>
   );
