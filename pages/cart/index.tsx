0557dd873404fe3a8821d50c3ac8dc1762c8d583
Siravit Phokeed <siravitsasa@gmail.com>
2023-08-17 00:36:10 +0700

    feat: Cart empty state

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -3,13 +3,19 @@ import PageHeader from "@/components/PageHeader";
 import ShopCart from "@/components/cart/ShopCart";
 import AppStateContext from "@/contexts/AppStateContext";
 import CartsContext from "@/contexts/CartsContext";
+import cn from "@/utils/helpers/cn";
 import { LangCode } from "@/utils/types/common";
-import { ContentLayout, Section } from "@suankularb-components/react";
+import {
+  Card,
+  ContentLayout,
+  Section,
+  Text,
+} from "@suankularb-components/react";
 import { GetStaticProps, NextPage } from "next";
+import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
 import { useContext, useEffect } from "react";
-import { useTranslation } from "next-i18next";
 
 const CartPage: NextPage = () => {
   const { t: tx } = useTranslation("common");
@@ -26,11 +32,29 @@ const CartPage: NextPage = () => {
       </Head>
       <PageHeader>รถเข็นของคุณ</PageHeader>
       <ContentLayout>
+        <Card appearance="outlined" className="mx-4 px-4 py-3 sm:mx-0">
+          <Text type="body-medium" element="p">
+            ถ้าคุณสั่งซื้อสินค้าไปแล้วโดยที่ไม่ได้เข้าสู่ระบบ
+            คุณจะสามารถดูใบเสร็จได้ที่อีเมลที่คุณกรอกไว้เมื่อสั่งซื้อ
+          </Text>
+        </Card>
         <Section element="ul">
-          {carts ? (
+          {carts?.length ? (
             carts.map((cart) => <ShopCart key={cart.shop.id} cart={cart} />)
           ) : (
-            <div className="h-36 animate-pulse rounded-md bg-surface-variant" />
+            <Card
+              appearance="outlined"
+              className={cn(`relative isolate mx-4 box-content grid h-[13rem]
+                place-content-center overflow-hidden px-4 py-3 sm:mx-0`)}
+            >
+              <Text
+                type="body-medium"
+                element="p"
+                className="text-center text-on-surface-variant"
+              >
+                ยังไม่มีสินค้าในรถเข็น
+              </Text>
+            </Card>
           )}
         </Section>
       </ContentLayout>
