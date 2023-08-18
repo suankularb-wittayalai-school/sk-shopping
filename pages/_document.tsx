5effbc56c2234369dd9296448dc78dd466d6adfa
Siravit Phokeed <siravitsasa@gmail.com>
2023-08-19 02:16:25 +0700

    feat: Shop OG

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,10 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta property="og:locale" content="th_TH" />
+        <meta property="og:site_name" content="SK Shopping" />
+      </Head>
       <body>
         <Main />
         <NextScript />
