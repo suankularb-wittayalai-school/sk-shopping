c2635365936c69931bcf138e8245e3724be23a76
Siravit Phokeed <siravitsasa@gmail.com>
2023-08-20 01:20:55 +0700

    fix: createJimmy param types

--- utils/helpers/createJimmy.ts
+++ utils/helpers/createJimmy.ts
@@ -4,18 +4,25 @@ import { logError } from "@/utils/helpers/logError";
 import { User } from "@/utils/types/user";
 import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 import { SupabaseClient } from "@supabase/supabase-js";
-import { NextApiRequest, NextApiResponse } from "next";
+import {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
 
 export default async function createJimmy(
-  req?: NextApiRequest,
-  res?: NextApiResponse,
+  req?: GetServerSidePropsContext["req"],
+  res?: GetServerSidePropsContext["res"],
 ) {
   const accessToken = req?.cookies["access_token"];
   let user: User | null = null;
   let sbStorage: SupabaseClient["storage"] | null = null;
 
   if (req && res) {
-    const { storage } = createPagesServerClient({ req, res });
+    const { storage } = createPagesServerClient({
+      req: req as NextApiRequest,
+      res: res as NextApiResponse,
+    });
     sbStorage = storage;
   }
 
