ce13c271b521cb03637c4ac3805b5df116673520
Siravit Phokeed <siravitsasa@gmail.com>
2023-08-21 21:18:15 +0700

    feat: time left in text

--- components/cart/checkout/PromptPayDialog.tsx
+++ components/cart/checkout/PromptPayDialog.tsx
@@ -2,7 +2,6 @@
 import cn from "@/utils/helpers/cn";
 import { logError } from "@/utils/helpers/logError";
 import useJimmy from "@/utils/helpers/useJimmy";
-import useLocale from "@/utils/helpers/useLocale";
 import { StylableFC } from "@/utils/types/common";
 import { CompactOrder, Order } from "@/utils/types/order";
 import { Dialog, Progress, Text } from "@suankularb-components/react";
@@ -31,22 +30,27 @@ const PromptPayDialog: StylableFC<{
   onClose: () => void;
   onSubmit: () => void;
 }> = ({ order, open, onClose, onSubmit, style, className }) => {
-  const locale = useLocale();
   const { t } = useTranslation("checkout", { keyPrefix: "payment.promptpay" });
 
   const jimmy = useJimmy();
 
+  // Every 1 second, reduce `timeLeft` by 1
   const [timeLeft, setTimeLeft] = useState(TIMEOUT_SEC);
   useEffect(() => {
     const interval = setInterval(() => {
+      // Calculate how much time is left until the order is canceled
+      // (Using Date to prevent interval freeze caused by tabbing out)
       const newTimeLeft =
         TIMEOUT_SEC -
         differenceInSeconds(new Date(), new Date(order.created_at));
-      console.log(newTimeLeft);
+
+      // If the window to pay has passed, close the Dialog
       if (newTimeLeft <= 0) {
         onClose();
         return;
       }
+
+      // Otherwise, set the new time left value and check if the order is paid
       setTimeLeft(newTimeLeft);
       (async () => {
         const { data, error } = await jimmy.fetch<CompactOrder>(
@@ -57,6 +61,8 @@ const PromptPayDialog: StylableFC<{
         else if (data.is_paid) onSubmit();
       })();
     }, 1000);
+
+    // Clean up
     return () => clearInterval(interval);
   }, []);
 
@@ -72,12 +78,27 @@ const PromptPayDialog: StylableFC<{
         <Text type="body-medium" className="grow text-on-surface-variant">
           {t("desc")}
         </Text>
-        <Progress
-          appearance="linear"
-          alt={t("countdownAlt")}
-          value={Math.ceil((timeLeft / TIMEOUT_SEC) * 100)}
-          visible
-        />
+        <div className="relative aspect-square">
+          <Progress
+            appearance="circular"
+            alt={t("countdownAlt")}
+            value={(timeLeft / TIMEOUT_SEC) * 100}
+            visible
+          />
+          <Text
+            type="overline"
+            className={cn(`absolute left-0 right-0 top-1/2 m-auto block
+              -translate-y-1/2 text-center text-primary
+              [font-feature-settings:"tnum"on,"lnum"on]`)}
+          >
+            {[
+              Math.floor(timeLeft / 60), // Minutes
+              timeLeft - Math.floor(timeLeft / 60) * 60, // Seconds
+            ]
+              .map((segment) => segment.toString().padStart(2, "0"))
+              .join(":")}
+          </Text>
+        </div>
       </div>
       <Image
         src={order.promptpay_qr_code_url!}
@@ -91,4 +112,3 @@ const PromptPayDialog: StylableFC<{
 };
 
 export default PromptPayDialog;
-
