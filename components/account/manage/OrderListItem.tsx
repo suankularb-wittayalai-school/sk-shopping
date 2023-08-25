4a5216e5d51a8885e251a992e2720cc24c6ac2d3
Siravit Phokeed <siravitsasa@gmail.com>
2023-08-26 01:11:02 +0700

    feat: adjust column sizes in Manage Orders

--- components/account/manage/OrderListItem.tsx
+++ components/account/manage/OrderListItem.tsx
@@ -8,6 +8,7 @@ import {
   Actions,
   Avatar,
   Button,
+  Columns,
   ListItem,
   ListItemContent,
   MaterialIcon,
@@ -37,88 +38,104 @@ const OrderListItem: StylableFC<{
       align="top"
       lines={3}
       style={style}
-      className={cn(
-        `!grid !pr-4
-        md:!grid-cols-[3rem,minmax(0,1fr),minmax(0,1fr),minmax(0,1fr),5.5rem]`,
-        className,
-      )}
+      className={cn(`!pr-4`, className)}
     >
-      <Avatar>
-        {order.buyer?.profile && (
-          <Image src={order.buyer.profile} width={48} height={48} alt="" />
-        )}
-      </Avatar>
-      <ListItemContent
-        title={order.receiver_name}
-        desc={sift([
-          new Date(order.created_at).toLocaleString(locale, {
-            day: "numeric",
-            month: "short",
-            year:
-              new Date(order.created_at).getFullYear() !==
-              new Date().getFullYear()
-                ? "numeric"
-                : undefined,
-            hour: "numeric",
-            minute: "numeric",
-          }),
-          order.is_paid && order.is_verified
-            ? `จ่าย ฿${order.total_price.toLocaleString(locale)} แล้ว`
-            : `ยังไม่ได้จ่าย ฿${order.total_price.toLocaleString(locale)}`,
-        ]).join(" • ")}
-        className="[&>span:first-child]:truncate"
-      />
-      <ListItemContent
-        title={<Text type="title-medium">สินค้าที่สั่งซื้อ</Text>}
-        desc={order.items
-          .map(({ item, amount }) => `${amount}×${item.name}`)
-          .join(", ")}
-        alt="สินค้าที่สั่งซื้อ"
-      />
-      <ListItemContent
-        title={
-          <Text type="title-medium">
-            {
+      <Columns columns={3} className="grow !items-stretch">
+        <div className="grid grid-cols-[3rem,1fr] gap-1">
+          <Avatar>
+            {order.buyer?.profile && (
+              <Image src={order.buyer.profile} width={48} height={48} alt="" />
+            )}
+          </Avatar>
+          <ListItemContent
+            title={order.receiver_name}
+            desc={sift([
+              new Date(order.created_at).toLocaleString(locale, {
+                day: "numeric",
+                month: "short",
+                year:
+                  new Date(order.created_at).getFullYear() !==
+                  new Date().getFullYear()
+                    ? "numeric"
+                    : undefined,
+                hour: "numeric",
+                minute: "numeric",
+              }),
+              order.is_paid && order.is_verified
+                ? `จ่าย ${order.total_price.toLocaleString(locale, {
+                    style: "currency",
+                    currency: "THB",
+                    maximumFractionDigits: 0,
+                  })} แล้ว`
+                : `ยังไม่ได้จ่าย ${order.total_price.toLocaleString(locale, {
+                    style: "currency",
+                    currency: "THB",
+                    maximumFractionDigits: 0,
+                  })}`,
+            ]).join(" • ")}
+            className="[&>span:first-child]:truncate"
+          />
+        </div>
+        <ListItemContent
+          title={<Text type="title-medium">สินค้าที่สั่งซื้อ</Text>}
+          desc={order.items
+            .map(({ item, amount }) => `${amount}×${item.name}`)
+            .join(", ")}
+          alt="สินค้าที่สั่งซื้อ"
+        />
+        <div
+          className={cn(`grid grid-cols-[1fr,calc(5.5rem+2px)] gap-4
+            sm:col-span-2 md:col-span-1`)}
+        >
+          <ListItemContent
+            title={
+              <Text type="title-medium">
+                {
+                  {
+                    pos: "รับที่หน้าร้าน",
+                    school_pickup: "รับที่โรงเรียน",
+                    delivery: "รับที่ที่อยู่…",
+                  }[order.delivery_type]
+                }
+              </Text>
+            }
+            desc={
+              order.delivery_type === "delivery"
+                ? [
+                    order.street_address_line_1,
+                    order.street_address_line_2?.replace("\n", " "),
+                    order.district,
+                    order.province,
+                    order.zip_code,
+                  ].join(" ")
+                : undefined
+            }
+            alt={
               {
                 pos: "รับที่หน้าร้าน",
                 school_pickup: "รับที่โรงเรียน",
                 delivery: "รับที่ที่อยู่…",
               }[order.delivery_type]
             }
-          </Text>
-        }
-        desc={
-          order.delivery_type === "delivery"
-            ? [
-                order.street_address_line_1,
-                order.street_address_line_2?.replace("\n", " "),
-                order.district,
-                order.province,
-                order.zip_code,
-              ].join(" ")
-            : undefined
-        }
-        alt={
-          {
-            pos: "รับที่หน้าร้าน",
-            school_pickup: "รับที่โรงเรียน",
-            delivery: "รับที่ที่อยู่…",
-          }[order.delivery_type]
-        }
-      />
-      <Actions className="self-end">
-        <Button
-          appearance="outlined"
-          icon={<MaterialIcon icon="receipt_long" />}
-          onClick={() => setDialogOpen(true)}
-        />
-        <ReceiptDialog
-          order={order}
-          open={dialogOpen}
-          onClose={() => setDialogOpen(false)}
-        />
-        <Button appearance="tonal" icon={<MaterialIcon icon="move_item" />} />
-      </Actions>
+          />
+          <Actions className="self-end">
+            <Button
+              appearance="outlined"
+              icon={<MaterialIcon icon="receipt_long" />}
+              onClick={() => setDialogOpen(true)}
+            />
+            <ReceiptDialog
+              order={order}
+              open={dialogOpen}
+              onClose={() => setDialogOpen(false)}
+            />
+            <Button
+              appearance="tonal"
+              icon={<MaterialIcon icon="move_item" />}
+            />
+          </Actions>
+        </div>
+      </Columns>
     </ListItem>
   );
 };
--- components/account/manage/SkeletonOrderListItem.tsx
+++ components/account/manage/SkeletonOrderListItem.tsx
@@ -1,7 +1,7 @@
 // Imports
 import cn from "@/utils/helpers/cn";
 import { StylableFC } from "@/utils/types/common";
-import { ListItem } from "@suankularb-components/react";
+import { Columns, ListItem } from "@suankularb-components/react";
 
 /**
  * A placeholder for Order List Item when Manage Orders is loading.
@@ -11,28 +11,33 @@ const SkeletonOrderListItem: StylableFC = ({ style, className }) => (
     align="top"
     lines={3}
     style={style}
-    className={cn(
-      `!grid animate-pulse !pr-4
-      md:!grid-cols-[3rem,minmax(0,1fr),minmax(0,1fr),minmax(0,1fr),5.5rem]`,
-      className,
-    )}
+    className={cn(`animate-pulse !pr-4`, className)}
   >
-    <div className="h-10 w-10 rounded-full bg-surface-variant" />
-    <div className="my-0.5 space-y-1">
-      <div className="h-5 w-36 rounded-xs bg-surface-variant" />
-      <div className="h-4 w-48 rounded-xs bg-surface-variant" />
-    </div>
-    <div className="my-0.5 space-y-1">
-      <div className="h-5 w-24 rounded-xs bg-surface-variant" />
-      <div className="h-4 w-56 rounded-xs bg-surface-variant" />
-    </div>
-    <div className="my-0.5">
-      <div className="h-5 w-20 rounded-xs bg-surface-variant" />
-    </div>
-    <div className="my-0.5 flex flex-row justify-end gap-2 self-end">
-      <div className="h-10 w-10 rounded-full bg-surface-variant" />
-      <div className="h-10 w-10 rounded-full bg-surface-variant" />
-    </div>
+    <Columns columns={3} className="grow !items-stretch">
+      <div className="grid grid-cols-[3rem,1fr] gap-1">
+        <div className="h-10 w-10 rounded-full bg-surface-variant" />
+        <div className="my-0.5 space-y-1">
+          <div className="h-5 w-36 rounded-xs bg-surface-variant" />
+          <div className="h-4 w-48 rounded-xs bg-surface-variant" />
+        </div>
+      </div>
+      <div className="my-0.5 space-y-1">
+        <div className="h-5 w-24 rounded-xs bg-surface-variant" />
+        <div className="h-4 w-56 rounded-xs bg-surface-variant" />
+      </div>
+      <div
+        className={cn(`grid grid-cols-[1fr,calc(5.5rem+2px)] gap-4
+          sm:col-span-2 md:col-span-1`)}
+      >
+        <div className="my-0.5">
+          <div className="h-5 w-20 rounded-xs bg-surface-variant" />
+        </div>
+        <div className="my-0.5 flex flex-row justify-end gap-2 self-end">
+          <div className="h-10 w-10 rounded-full bg-surface-variant" />
+          <div className="h-10 w-10 rounded-full bg-surface-variant" />
+        </div>
+      </div>
+    </Columns>
   </ListItem>
 );
 
